a0e69b3885c063f8c762f431658fd53bea0180b0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-15 20:47:32 +0200

    Added BaseModel

---
+++ EStudy/EStudy/EStudy.Domain/Models/BaseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EStudy.Domain.Models
+{
+    public class BaseModel<TypeId>
+    {
+        [Key]
+        public TypeId Id{ get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [MinLength(3), MaxLength(50)]
+        public string CreatedFromIP { get; set; }
+        public int CreatedByUserId { get; set; }
+
+
+
+
+        [Required]
+        public bool IsEdit { get; set; } = false;
+        public DateTime? DateLastEdit { get; set; }
+        public int? EditedByUserId { get; set; }
+        public string History { get; set; }
+    }
+}
