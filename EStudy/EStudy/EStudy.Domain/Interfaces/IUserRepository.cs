44bf15f298da9424ed6408ccb22fff9fd755e88f
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-15 21:46:54 +0200

    Released SearchAsync

--- EStudy/EStudy/EStudy.Domain/Interfaces/IUserRepository.cs
+++ EStudy/EStudy/EStudy.Domain/Interfaces/IUserRepository.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EStudy.Domain.Interfaces
 {
     public interface IUserRepository : IRepository<User>
     {
-
+        Task<List<User>> SearchAsync(string name, int count, int skip);
     }
 }
--- EStudy/EStudy/EStudy.Infrastructure.Data/Repositories/UserRepository.cs
+++ EStudy/EStudy/EStudy.Infrastructure.Data/Repositories/UserRepository.cs
@@ -1,13 +1,29 @@
 ﻿using EStudy.Domain.Interfaces;
 using EStudy.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EStudy.Infrastructure.Data.Repositories
 {
     public class UserRepository : Repository<User>, IUserRepository
     {
-
+        public async Task<List<User>> SearchAsync(string name, int count, int skip)
+        {
+            return await db.Users.AsNoTracking()
+                .Where(d => d.FirstName.Contains(name) || d.LastName.Contains(name))
+                .Select(d => new User
+                {
+                    FirstName = d.FirstName,
+                    LastName = d.LastName,
+                    Username = d.Username,
+                    Avatar50 = d.Avatar50
+                })
+                .Skip(skip).Take(count)
+                .ToListAsync();
+        }
     }
 }
