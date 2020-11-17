735f0ade7de830f39d6d132c3be78200eb7f8c93
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-17 18:27:59 +0200

    Added some models

--- EStudy/EStudy/EStudy.Application/ViewModels/Group/EmailEditModel.cs
+++ EStudy/EStudy/EStudy.Application/ViewModels/Group/EmailEditModel.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 namespace EStudy.Application.ViewModels.Group
 {
-    public class EmailViewModel
+    public class EmailEditModel : RequestModel
     {
+        [Required]
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        [Required, MinLength(3), MaxLength(50)]
         public string Title { get; set; }
+        [Required, EmailAddress, MinLength(7), MaxLength(100)]
         public string Address { get; set; }
+        [Required, MinLength(4), MaxLength(100)]
         public string Password { get; set; }
-        public GroupViewModel Group { get; set; }
+        public int GroupId { get; set; }
     }
 }
