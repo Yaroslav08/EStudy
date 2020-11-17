a10d679b3912e789484bc80c3bd52b1d004af039
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-17 13:08:17 +0200

    Added ViewModels

---
+++ EStudy/EStudy/EStudy.Application/ViewModels/Specialty/SpecialtyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EStudy.Application.ViewModels.Department;
+using EStudy.Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EStudy.Application.ViewModels.Specialty
+{
+    public class SpecialtyViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Shifr { get; set; }
+        public string Code { get; set; }
+        public TypeDiploma Qualification { get; set; }
+        public string EducationalProgram { get; set; }
+        public string ProfessionalQualification { get; set; }
+        public string About { get; set; }
+        public DepartmentViewModel Department { get; set; }
+    }
+}
