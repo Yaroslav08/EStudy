ecc22e1c6dca175e752ff582a5a773668bcd11ed
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-12 14:32:15 +0200

    Added Genders entities

---
+++ EStudy/EStudy/EStudy.Constants/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EStudy.Constants
+{
+    public class Gender
+    {
+        public int Value { get; set; }
+        public string UkrName { get; set; }
+    }
+
+    public class Genders
+    {
+        private List<Gender> genders;
+
+        public List<Gender> GetGenders()
+        {
+            if (genders == null || genders.Count == 0)
+            {
+                genders = new List<Gender>
+                {
+                    new Gender
+                    {
+                        Value = 0,
+                        UkrName = "Жінка"
+                    },
+                    new Gender
+                    {
+                        Value = 1,
+                        UkrName = "Чоловік"
+                    }
+                };
+            }
+            return genders;
+        }
+    }
+}
