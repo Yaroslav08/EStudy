44d30078f79b3ea20a979992b705871be40f3989
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-12 13:58:46 +0200

    Added DisplayName to UserEditModel

--- EStudy/EStudy/EStudy.Application/ViewModels/User/UserEditModel.cs
+++ EStudy/EStudy/EStudy.Application/ViewModels/User/UserEditModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,25 +12,35 @@ namespace EStudy.Application.ViewModels.User
         [Required]
         public int Id { get; set; }
         [Required, MinLength(3), MaxLength(25)]
+        [DisplayName("Ім'я")]
         public string FirstName { get; set; }
         [MinLength(3), MaxLength(30)]
+        [DisplayName("По-батькові")]
         public string MiddleName { get; set; }
         [Required, MinLength(3), MaxLength(25)]
+        [DisplayName("Прізвище")]
         public string LastName { get; set; }
         [MinLength(4), MaxLength(25)]
+        [DisplayName("Нікнейм")]
         public string Username { get; set; }
         [MinLength(5), MaxLength(500)]
+        [DisplayName("Про себе")]
         public string About { get; set; }
+        [DisplayName("Дата народження")]
         public DateTime? Born { get; set; }
         [MinLength(4), MaxLength(250)]
+        [DisplayName("Ваше місцезнаходження (або місто народження)")]
         public string Location { get; set; }
         [MinLength(10), MaxLength(250)]
+        [DisplayName("Посилання на аватарку")]
         public string Avatar { get; set; }
         [MinLength(3), MaxLength(25), Phone]
+        [DisplayName("Телефон")]
         public string Phone { get; set; }
         [Required]
         public bool IsShowPhone { get; set; }
         [MinLength(5), MaxLength(100), EmailAddress]
+        [DisplayName("Електронна пошта")]
         public string Email { get; set; }
         [Required]
         public bool IsShowEmail { get; set; }
