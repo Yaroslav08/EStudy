7f0b460af5ff565bb52fda7de7df40a47fc3d39f
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-16 13:37:14 +0200

    Change PasswordChangeViewModel attrbs

--- EStudy/EStudy/EStudy.Application/ViewModels/Auth/PasswordChangeViewModel.cs
+++ EStudy/EStudy/EStudy.Application/ViewModels/Auth/PasswordChangeViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,11 @@ namespace EStudy.Application.ViewModels.Auth
     public class PasswordChangeViewModel
     {
         public int UserId { get; set; }
+        [Required(ErrorMessage = "Пароль обов'язковий"), MinLength(8, ErrorMessage = "Мінімальна довжина 8 символів"), MaxLength(25, ErrorMessage = "Максимальна довжина 25 символів")]
+        [DisplayName("Старий пароль")]
         public string OldPassword { get; set; }
+        [Required(ErrorMessage = "Пароль обов'язковий"), MinLength(8, ErrorMessage = "Мінімальна довжина 8 символів"), MaxLength(25, ErrorMessage = "Максимальна довжина 25 символів")]
+        [DisplayName("Новий пароль")]
         public string NewPassword { get; set; }
     }
 }
