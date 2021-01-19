85edc222fb060d72458ecdb18ccfc3c098b5544a
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-19 19:16:52 +0200

    Changed NotificationHub

--- EStudy/EStudy/EStudy.MVC/Notification/NotificationHub.cs
+++ EStudy/EStudy/EStudy.MVC/Notification/NotificationHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,10 @@ namespace EStudy.MVC.Notification
 {
     public class NotificationHub : Hub
     {
-        private int onlineUsers = 0;
-        public async Task SendToAll(string message)
+        [Authorize(Roles = "Admin")]
+        public async Task Notify(string message)
         {
             await Clients.All.SendAsync("Notify", message);
         }
-
-        public override async Task OnConnectedAsync()
-        {
-            onlineUsers++;
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            onlineUsers--;
-            await base.OnDisconnectedAsync(exception);
-        }
-
-        public int OnlineUsers => onlineUsers;
     }
 }
