c7e4d2b1aa6c46fe3a75ba4ab952baf6bfc2a69f
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-21 18:48:29 +0200

    Added BaseController for api

---
+++ EStudy/EStudy/EStudy.MVC/API/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace EStudy.MVC.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public int GetId() => Convert.ToInt32(User.Identity.Name);
+        public string GetIP() => HttpContext.Connection.RemoteIpAddress.ToString();
+        public string GetRole() => User.Claims.FirstOrDefault(d => d.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+        public string GetFullName() => User.Claims.FirstOrDefault(d => d.Type == "Fullname").Value;
+        public string GetUsername()
+        {
+            var claims = User.Identities.First();
+            var claim = claims.Claims.FirstOrDefault(d => d.Type == "Username");
+            if (claim == null)
+                return "";
+            return claim.Value;
+        }
+    }
+}
