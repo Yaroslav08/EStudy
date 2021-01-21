57084cd15e5ef96267c825d4aba11cdb4ce6f0be
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-01-21 15:16:02 +0200

    Added class for valid base and twitter link on account user

---
+++ EStudy/EStudy/EStudy.Constants/SocialLinksValidate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EStudy.Constants
+{
+    public class SocialLinksValidate
+    {
+        private bool BaseUrlIsValid(string url)
+        {
+            if (url.StartsWith("https://") || url.StartsWith("http://"))
+                return true;
+            return false;
+        }
+
+        public bool TwitterIsValid(string url)
+        {
+            if (!BaseUrlIsValid(url))
+                return false;
+
+            if (url.StartsWith("https://twitter.com") || url.StartsWith("https://api.twitter.com"))
+                return true;
+            return false;
+        }
+    }
+}
